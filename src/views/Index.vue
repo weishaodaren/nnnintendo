0f0cd5b73de61c9686c8f56625c5a2d712f113bb
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-03-15 20:54:03 +0800

    new

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,34 @@
 <template>
   <div class="">
     <el-container>
+      <!-- 头部 -->
       <el-header>
+        
+<el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#fff"
+  text-color="#000"
+  active-text-color="#e60012">
+  <el-menu-item index="1">处理中心</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+  <el-menu-item index="4"><a href="" target="_blank">订单管理</a></el-menu-item>
+</el-menu>
+
         <el-main>
           <el-footer></el-footer>
         </el-main>
@@ -20,3 +47,18 @@ export default {
 
 </style>
 
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
