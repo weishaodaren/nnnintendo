c971af2b34e326bf374a55ebc5606c1135b3456c
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-03-20 09:37:32 +0800

    debug

--- src/components/NinTopics.vue
+++ src/components/NinTopics.vue
@@ -5,14 +5,14 @@
         <h5>TOPICS</h5>
         <el-row>
           <el-col :sm=12 :md=8 :lg=6 
-          v-for='(i,index) of items.slice(5,13)' :key='index'
-          @mousemove="nin_move(index)" 
-          :class="{'nin_bind_style':isA==index}">
-            <div>
+          v-for='(i,index) of items.slice(5,13)' :key='index'>
+            <div> 
+              <a href="javascript:;">
               <img :src="i.imgUrl"/>
                <h6>{{i.tname}}</h6> 
+               </a>
                <br><br>
-               <p>{{Gamer}}</p>
+               <p>{{i.Gamer}}</p>
                <p>{{i.publishTime}}</p>
             </div>
           </el-col>             
@@ -28,19 +28,16 @@
 export default {
     data(){
       return{
-        items:[],
-        Gamer:'Nintendo Switch',
-        isA:''
+        items:[]
       }
     },
     methods:{
-      nin_move:function(index){
+      nin_move:function(){
       
       }
     },
     created(){
       this.$axios.get(this.$store.state.globalSettings.apiUrl+'index_middle').then((res)=>{
-        console.log(res.data);
         this.items=res.data;
       }).catch((err)=>{
         console.log(err);
@@ -72,6 +69,9 @@ export default {
           height: 300px;
           padding-left:20px;
           padding-right:10px; 
+          a{
+            text-decoration-line: none;
+            color: #000;
           img{
             width:95%;
             height:140px;
@@ -80,18 +80,16 @@ export default {
           h6{
             font-weight: bolder;
           }
-          p{
-            opacity: 0.7;
-            font-size: 10px;
+          }:hover{
+             color:#e60012;
+              opacity: 0.9;
           }
+         
         }
     }
-.nin_bind_style{
-  img{
-    opacity: 0.5;
-  }
-  h6{
-    color:#e60012;
-  }
-}
+    div p{
+            opacity: 0.7;
+            font-size: 10px;
+            color:black
+    }
 </style>
